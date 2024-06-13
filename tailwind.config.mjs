aa544e3c036d1ff2e39c9a5685c9e4119330ff93
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-06-13 12:05:08 -0500

    [CHANGE] .svg to .astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				"currentColor" : "#EEEEEE",
+				'primary' : '#9b20b0ce',
+				'dark': '#232830;',
+				'very-light-pink': '#c7c7c7;',
+				'text-input-field': '#f7f7f7;',
+				'hospital-green': '#acd9b2;',
+			}
+		},
 	},
 	plugins: [],
 }

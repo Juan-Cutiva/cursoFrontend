053286c027b38609724d2484ed71f80949e0f6da
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-06-18 11:53:58 -0500

    [FINISH] Team

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,14 +13,19 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 
 const navigation = [
 	{ name: 'Home', href: '/', current: true },
-	{ name: 'Team', href: '#', current: false },
+	{ name: 'Team', href: '/Team', current: false },
 	{
 		name: 'Repository',
 		href: 'https://github.com/Juan-Cutiva/cursoFrontend',
 		current: false,
 		target: '_blank'
 	},
-	{ name: 'Calendar', href: '#', current: false }
+	{
+		name: 'PortFolio',
+		href: 'https://portfoliocuti.netlify.app/',
+		current: false,
+		target: '_blank'
+	}
 ];
 
 function classNames(...classes) {

d2b6d2f800e49a1234e73427f3e539d6bd651558
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-06-21 14:06:15 -0500

    [FIX] NavBar_2

--- src/components/NavBar_2.jsx
+++ src/components/NavBar_2.jsx
@@ -154,7 +154,7 @@ export default function Example() {
 										<MenuButton className="relative flex text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
 											<span className="absolute -inset-1.5" />
 											<span className="sr-only">Open user menu</span>
-											<p class="text-verde font-bold">Sign in</p>
+											<p class="text-verde font-bold">Log in</p>
 										</MenuButton>
 										<img className="h-6 w-6 ml-4" src="\Platzi Yard Sale\Platzi_YardSale_Icons\icon_shopping_cart_notification.svg" alt="icon_shopping_cart_notification" />
 									</div>
@@ -167,7 +167,7 @@ export default function Example() {
 										leaveTo="transform opacity-0 scale-95"
 									>
 										<MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-											<MenuItem>
+										<MenuItem>
 												{({ focus }) => (
 													<a
 														href="#"
@@ -176,7 +176,7 @@ export default function Example() {
 															'block px-4 py-2 text-sm text-gray-700'
 														)}
 													>
-														Prueba
+														Sign In
 													</a>
 												)}
 											</MenuItem>
@@ -189,7 +189,7 @@ export default function Example() {
 															'block px-4 py-2 text-sm text-gray-700'
 														)}
 													>
-														Sign out
+														Create Your Account
 													</a>
 												)}
 											</MenuItem>

d9b18f6b04df76989d965186abad149be2f86dc1
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-06-24 19:52:14 -0500

    [BUILD] Page Description

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -2,41 +2,40 @@ import { Fragment } from 'react';
 
 import Prueba from './texto.json';
 
-// const Result = Prueba.filter((item) => item.key == 1);
-const Result2 = Prueba[17]
-// console.log(Result);
-console.log(Result2)
+
+const Result2 = Prueba[1];
+
+console.log(Result2);
 
 export default function Example() {
 	return (
 		<>
-			<div className="mx-1 my-2 flex items-center justify-center sm:mx-3">
-						<a
-							key={Result2.key}
-							href={Result2.href}
-							rel="noopener noreferrer"
-							aria-current={Result2.current ? 'page' : undefined}
-						>
-							<div className="flex items-center justify-center">
-								<img
-									className="max-h-60 min-h-60 min-w-[23rem] max-w-[23rem] rounded-3xl drop-shadow-lg sm:max-h-40 sm:min-h-40 sm:min-w-56 sm:max-w-48"
-									src={Result2.src}
-									alt={Result2.alt}
-								/>
-							</div>
-							<div className="mt-2 flex flex-row justify-between">
-								<article className="mr-4 flex flex-col">
-									<p className="font-bold">{Result2.price}</p>
-									<p className="font-medium">{Result2.name}</p>
-								</article>
-								<div>
-									<a>
-										<img src={Result2.srcButton} alt={Result2.altButton} className="drop-shadow-md" />
-									</a>
-								</div>
-							</div>
-						</a>
+			<div className="flex items-center justify-center">
+				<a
+					key={Result2.key}
+					href={Result2.href}
+					rel="noopener noreferrer"
+					aria-current={Result2.current ? 'page' : undefined}
+				>
+					<div className=" flex items-center justify-center">
+						<img className="max-w-[full]  rounded-b-2xl sm:h-[24rem]" src={Result2.src} alt={Result2.alt} />
 					</div>
+					<div className="mt-10 flex flex-col justify-between">
+						<article className="mr-4 mx-4 flex flex-col">
+							<p className="font-bold text-xl">{Result2.price}</p>
+							<p className="font-medium text-gray-600 mt-2">{Result2.name}</p>
+						</article>
+						<div className="w-full mt-4">
+							<h1 className='mx-4'>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus consequatur
+								cupiditate at temporibus sunt molestiae perspiciatis laboriosam vitae veniam facere,
+								quibusdam voluptate architecto adipisci cumque aut error. Repellendus, iusto ipsa!
+							</h1>
+							
+						</div>
+					</div>
+				</a>
+			</div>
 		</>
 	);
 }
